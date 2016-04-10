4a57ee8f8cecc0dedb07a4c0eae3a4160809c74c
cyberash <cyberash@mail.ru>
2016-04-10 12:04:05 +0300

    code upload files php

---
+++ index.php
@@ -0,0 +1,7 @@
+<form action="./upload.php" method="post" enctype="multipart/form-data">
+
+<p><input type="file" name="userfile" id="file"></p>
+
+<p><input type="submit" value="Загрузить фаил"></p>
+
+</form>
